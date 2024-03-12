53fd05a333ee118f74083a6dd0d5544b8891b4c3
DryCrade <144786409+DryCrade@users.noreply.github.com>
2024-03-12 14:05:48 +0700

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+import static java.lang.System.exit;
+
+public class Palindrome {
+    public static String checkPalindrome (String str) {
+        ArrayDeque<Character> queue = new ArrayDeque<>();
+        for (int i = 0; i < str.length() ; i++) {
+            char c = str.charAt(i);
+            queue.add(c);
+        }
+
+        System.out.println(queue.peekFirst());
+        while (queue.size() > 1) {
+            if (!Objects.equals(queue.pollFirst(), queue.pollLast())) {
+                return ("This is not a palindrome");
+            }
+        }
+        return ("This is a palindrome");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine();
+
+        System.out.println(checkPalindrome(input));
+    }
+}
+
